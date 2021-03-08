23ff9cd9bcfc28148a9796a97d25b2bf042af9da
Evan Mielak <evanmielak@gmail.com>
2021-03-07 23:22:33 -0600

    Added background music, sound effect for building and destroying buildings.

--- TileMap.gd
+++ TileMap.gd
@@ -7,11 +7,9 @@ extends TileMap
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	# Sample cell initialization
-	#for x in range(0, 15):
-	#	for y in range(0, 8):
-	#		self.set_cell(x, y, (x + y) % 27)
+# func _ready():
+	# pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +21,19 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		var cellv = (event.position / 64).floor()
 		var id = self.get_cellv(cellv)
+		
 		# Increment cell ID on LMB, otherwise clear cell
 		var new_id = (id + 1) % 27 if event.button_index == 1 else -1
 		self.set_cellv(cellv, new_id)
+		
+		# Initialize and play the sound for building a building.
+		if event.button_index == 1:
+			var building = AudioStreamPlayer.new()
+			self.add_child(building)
+			building.stream = load("building.wav")
+			building.play()
+		else:
+			var building_destroy = AudioStreamPlayer.new()
+			self.add_child(building_destroy)
+			building_destroy.stream = load("building_destroy.wav")
+			building_destroy.play()
