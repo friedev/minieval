f24b124725bc302b8c0c189e8126d33f9a276586
Aaron Friesen <maugrift@maugrift.com>
2021-03-07 21:58:11 -0600

    Add a temporary pixel art tileset

--- TileMap.gd
+++ TileMap.gd
@@ -7,11 +7,10 @@ extends TileMap
 
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-	# Sample cell initialization
-	#for x in range(0, 15):
-	#	for y in range(0, 8):
-	#		self.set_cell(x, y, (x + y) % 27)
+func _ready():
+	for x in range(0, 64):
+		for y in range(0, 32):
+			self.set_cell(x, y, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,8 +20,8 @@ extends TileMap
 func _input(event):
 	# Handle mouse clicks (and not unclicks)
 	if event is InputEventMouseButton and event.pressed:
-		var cellv = (event.position / 64).floor()
+		var cellv = (event.position / 32).floor()
 		var id = self.get_cellv(cellv)
 		# Increment cell ID on LMB, otherwise clear cell
-		var new_id = (id + 1) % 27 if event.button_index == 1 else -1
+		var new_id = (id + 1) % 16 if event.button_index == 1 else 0
 		self.set_cellv(cellv, new_id)
