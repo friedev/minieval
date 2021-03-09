4e6bb53602d2c68bdc58ec6119b249d4f8c9feef
Kalen Wallin <kwallin10@huskers.unl.edu>
2021-03-08 18:15:47 -0600

    Added Title Screen
    Game now starts on Title Screen with two options:
    -Play
    -Exit Game
    
    Added Title Screen button to Pause Menu. So you don't have to exit game and re-open game to return to the main menu.
    
    Additional Changes:
    -Turned the pause menu into its own scene for modularity.
    -Refactored Pause.gd to PauseUIScript.gd to create naming consistency among UI Scripts.

--- Interface/PauseUIScript.gd
+++ Interface/PauseUIScript.gd
@@ -1,5 +1,6 @@
 extends Control
 
+#Make pause menu appear when user presses pause (Escape)
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
@@ -13,3 +14,9 @@ func _on_ResumeButton_pressed():
 
 func _on_ExitGameButton_pressed():
 	get_tree().quit()
+
+
+func _on_ReturnToTitleButton_pressed():
+	var new_pause_state = not get_tree().paused
+	get_tree().paused = new_pause_state
+	get_tree().change_scene("res://Interface/Title.tscn")
