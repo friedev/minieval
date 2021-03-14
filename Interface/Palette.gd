ddbc9255dcabadd39adab6a7ab723378a709585d
Aaron Friesen <maugrift@maugrift.com>
2021-03-13 20:22:52 -0600

    Add keyboard shortcuts to building palette

--- Interface/Palette.gd
+++ Interface/Palette.gd
@@ -4,9 +4,13 @@ signal palette_selection(id)
 
 func _input(event):
 	# Handle mouse clicks (and not unclicks)
+	var cellv
 	if event is InputEventMouseButton and event.pressed:
-		var cellv = ((event.position - self.global_position) / 64).floor()
-		var id = self.get_cellv(cellv)
-		if id != INVALID_CELL:
-			emit_signal("palette_selection", id)
-	
+		cellv = ((event.position - self.global_position) / 64).floor()
+	elif event is InputEventKey and event.pressed:
+		cellv = Vector2(event.scancode - KEY_1, 0)
+	else:
+		return
+	var id = self.get_cellv(cellv)
+	if id != INVALID_CELL:
+		emit_signal("palette_selection", id)
