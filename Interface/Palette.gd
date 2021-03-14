681ba20d9a9e6f733aaa48e29923fc1cd58799b5
Aaron Friesen <maugrift@maugrift.com>
2021-03-13 19:50:15 -0600

    Implement building palette

---
+++ Interface/Palette.gd
@@ -0,0 +1,11 @@
+extends TileMap
+
+signal palette_selection(id)
+
+func _input(event):
+	# Handle mouse clicks (and not unclicks)
+	if event is InputEventMouseButton and event.pressed:
+		var cellv = ((event.position - self.global_position) / 64).floor()
+		var id = self.get_cellv(cellv)
+		if id != INVALID_CELL:
+			emit_signal("palette_selection", id)
--- TileMap.gd
+++ TileMap.gd
@@ -1,30 +1,29 @@
 extends TileMap
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var selected_building = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node(@"/root/Root/Palette/Menu/TileMap").connect("palette_selection", self, "_select_building")
 	for x in range(0, 64):
 		for y in range(0, 32):
 			self.set_cell(x, y, 0)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _input(event):
+func _unhandled_input(event):
 	# Handle mouse clicks (and not unclicks)
 	if event is InputEventMouseButton and event.pressed:
 		var cellv = (event.position / 32).floor()
 		var id = self.get_cellv(cellv)
 		
 		# Increment cell ID on LMB, otherwise clear cell
-		var new_id = (id + 1) % 16 if event.button_index == 1 else 0
+		var new_id
+		if event.button_index == 1:
+			new_id = self.selected_building
+		elif event.button_index == 2:
+			new_id = 0
 		self.set_cellv(cellv, new_id)
 		
 		# Initialize and play the sound for building a building.
@@ -38,3 +37,8 @@ func _input(event):
 			self.add_child(building_destroy)
 			building_destroy.stream = load("building_destroy.wav")
 			building_destroy.play()
+
+
+func _select_building(id):
+	self.selected_building = id
+	print(self.selected_building)
