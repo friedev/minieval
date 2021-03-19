07b3213fc812afbf2d8e187f5098d2ecb8f88b11
Aaron Friesen <maugrift@maugrift.com>
2021-03-19 12:15:46 -0500

    Implement victory points

--- TileMap.gd
+++ TileMap.gd
@@ -5,83 +5,101 @@ class Building:
 	var base_cost
 	var cost
 	var cost_increment
+	var base_vp
+	var vp
+	var vp_increment
 	var radius
-	var interactions
+	var currency_interactions
+	var vp_interactions
 	
-	func _init(base_cost, cost_increment, radius, interactions):
+	func _init(base_cost, cost_increment, base_vp, vp_increment, radius, currency_interactions, vp_interactions):
 		self.base_cost = base_cost
 		self.cost = base_cost
 		self.cost_increment = cost_increment
+		self.base_vp = base_vp
+		self.vp = base_vp
+		self.vp_increment = vp_increment
 		self.radius = radius
-		self.interactions = interactions
+		self.currency_interactions = currency_interactions
+		self.vp_interactions = vp_interactions
 
 
 var buildings = [
 	null,
-	Building.new(1, 0.25, 1, { # 1: Wooden hut
+	Building.new(1, 0.25, 1, 0, 1, { # 1: Wooden hut
 		1: 1,  # Wooden hut
 		2: 2,  # Wooden house
 		3: -1, # Stone hut
 		4: -2, # Stone house
 		5: 1,  # Castle
+	}, {
 	}),
-	Building.new(2, 0.5, 1, {  # 2: Wooden house
+	Building.new(2, 0.5, 1, 0, 1, {  # 2: Wooden house
 		1: 2,  # Wooden hut
 		2: 4,  # Wooden house
 		3: -2, # Stone hut
 		4: -4, # Stone house
 		5: 1,  # Castle
+	}, {
 	}),
-	Building.new(3, 0.25, 2, {  # 3: Stone hut
+	Building.new(3, 0.25, 2, 0, 2, {  # 3: Stone hut
 		1: -1, # Wooden hut
 		2: -2, # Wooden house
 		3: 1,  # Stone hut
 		4: 2,  # Stone house
 		5: 2,  # Castle
+	}, {
 	}),
-	Building.new(4, 0.5, 2, { # 4: Stone house
+	Building.new(4, 0.5, 2, 0, 2, { # 4: Stone house
 		1: -2, # Wooden hut
 		2: -4, # Wooden house
 		3: 2,  # Stone hut
 		4: 4,  # Stone house
 		5: 2,  # Castle
+	}, {
 	}),
-	Building.new(10, 10, 10, {  # 5: Castle
+	Building.new(10, 10, 10, 0, 10, {  # 5: Castle
 		1: 1,   # Wooden hut
 		2: 1,   # Wooden house
 		3: 2,   # Stone hut
 		4: 2,   # Stone house
 		5: -10, # Castle
 		6: 5,   # Tower
+	}, {
 	}),
-	Building.new(5, 5, 5, {  # 6: Tower
+	Building.new(5, 5, 5, 0, 5, {  # 6: Tower
 		5: 5,  # Castle
 		6: -5, # Tower
+	}, {
 	}),
 	null, # 7: Wall
-	Building.new(5, 2.5, 5, {  # 8: Fountain
+	Building.new(5, 2.5, 2, 0, 5, {  # 8: Fountain
 		1: 1, # Wooden hut
 		2: 1, # Wooden house
 		3: 1, # Stone hut
 		4: 1, # Stone house
+	}, {
 	}),
-	Building.new(5, 2.5, 5, {  # 9: Well
+	Building.new(5, 2.5, 2, 0, 5, {  # 9: Well
 		1: 2,  # Wooden hut
 		2: 2,  # Wooden house
 		3: -2, # Stone hut
 		4: -2, # Stone house
+	}, {
 	}),
 ]
 
 
 var currency = 10
+var vp = 0
 var selected_building = 1
 
+var label_format = "Currency: %d\nVictory Points: %d"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node(@"/root/Root/Palette/Menu/TileMap").connect("palette_selection", self, "_select_building")
-	get_node(@"/root/Root/CurrencyLayer/CurrencyLabel").text = "Currency: %d" % currency
+	get_node(@"/root/Root/CurrencyLayer/CurrencyLabel").text = label_format % [currency, vp]
 	for x in range(0, 128):
 		for y in range(0, 128):
 			self.set_cell(x, y, 0)
@@ -112,23 +130,27 @@ func _unhandled_input(event):
 				currency -= floor(building.cost)
 			else:
 				return # Play error sound
-
-			building.cost += building.cost_increment
-
+			
+			vp += floor(building.vp)
+			
 			# Give currency based on nearby buildings
 			# TODO prevent currency from going negative if net currency change is negative
 			for x in range(max(0, cellv.x - building.radius), min(127, cellv.x + building.radius + 1)):
 				for y in range(max(0, cellv.y - building.radius), min(127, cellv.y + building.radius + 1)):
 					var neighbor_id = self.get_cell(x, y)
-					var currency_change = building.interactions.get(neighbor_id, 0)
-					currency += currency_change
-
-			get_node(@"/root/Root/CurrencyLayer/CurrencyLabel").text = "Currency: %d" % currency
-
+					currency += building.currency_interactions.get(neighbor_id, 0)
+					vp += building.vp_interactions.get(neighbor_id, 0)
+			
+			building.cost += building.cost_increment
+			building.vp += building.vp_increment
+			
+			get_node(@"/root/Root/CurrencyLayer/CurrencyLabel").text = label_format % [currency, vp]
+			
 			$BuildingPlaceSound.play()
 		else:
 			var building = buildings[id]
 			building.cost -= building.cost_increment
+			building.vp -= building.vp_increment
 			
 			$BuildingDestroySound.play()
 		
