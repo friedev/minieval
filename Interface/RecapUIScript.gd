33043ff5f433a557decb7f5f010b39a50fd1a426
Kalen Wallin <kwallin10@huskers.unl.edu>
2021-03-21 21:10:40 -0500

    Improved Stats overlay

--- Interface/RecapUIScript.gd
+++ Interface/RecapUIScript.gd
@@ -1,22 +1,18 @@
 extends Control
 
-#Make score report overlay appear when user holds shift
+var game_over = false
+
+#Make end game recap report appear
+#when game ends (TODO: make game endable/losable)
 func _input(event):
 	var palette = get_node(@"/root/Root/Palette/Menu")
 	var currencyLayer = get_node(@"/root/Root/CurrencyLayer/CurrencyLabel")
-	if event.is_action_pressed("score_report"):
-		#TODO: make palette and currency/vp label disappear
+	if game_over:
+		visible = true
 		palette.visible = false
 		currencyLayer.visible = false
-		update_currency()
-		visible = true
-	elif event.is_action_released("score_report"):
-		palette.visible = true
-		currencyLayer.visible = true
-		visible = false
 
-#update currency
-func update_currency():
+func update_labels():
 	var tilemap = get_node(@"/root/Root/TileMap")
 	get_node("Stats/CurrencyCountLabel").text = "%d" % tilemap.currency
 	get_node("Stats/VPCountLabel").text = "%d" % tilemap.vp
