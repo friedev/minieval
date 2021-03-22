a60a0e0a0b995900c5da4e14876b761385e02741
Aaron Friesen <maugrift@maugrift.com>
2021-03-21 23:43:22 -0500

    Fix issues in RecapUIScript

--- Interface/RecapUIScript.gd
+++ Interface/RecapUIScript.gd
@@ -6,14 +6,15 @@ var game_over = false
 #when game ends (TODO: make game endable/losable)
 func _input(event):
 	var palette = get_node(@"/root/Root/Palette/Menu")
-	var currencyLayer = get_node(@"/root/Root/CurrencyLayer/CurrencyLabel")
+	var ui_text_layer = get_node(@"/root/Root/UITextLayer")
 	if game_over:
 		visible = true
 		palette.visible = false
-		currencyLayer.visible = false
+		for child in ui_text_layer.get_children():
+			child.visible = false
 
 func update_labels():
 	var tilemap = get_node(@"/root/Root/TileMap")
-	get_node("Stats/CurrencyCountLabel").text = "%d" % tilemap.currency
-	get_node("Stats/VPCountLabel").text = "%d" % tilemap.vp
-	get_node("Stats/BuildingsPlacedCountLabel").text = "%d" % tilemap.buildings_placed
+	get_node("Stats/CurrencyCountLabel").text = str(tilemap.currency)
+	get_node("Stats/VPCountLabel").text = str(tilemap.vp)
+	get_node("Stats/BuildingsPlacedCountLabel").text = str(tilemap.buildings_placed)
