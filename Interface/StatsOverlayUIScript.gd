b409e862dec0c491843d8e2d6a51ad3e4ccbd608
Kalen Wallin <kwallin10@huskers.unl.edu>
2021-03-25 11:23:20 -0500

    Game paused & Preview cleared during Stats Overlay

--- Interface/StatsOverlayUIScript.gd
+++ Interface/StatsOverlayUIScript.gd
@@ -2,15 +2,21 @@ extends Control
 
 # Make score report overlay appear when user holds shift
 func _input(event):
+	var tilemap = get_node(@"/root/Root/TileMap")
 	var palette = get_node(@"/root/Root/Palette/Menu")
 	var ui_text_layer = get_node(@"/root/Root/UITextLayer")
 	if event.is_action_pressed("score_report"):
+		tilemap._clear_preview()
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
 		palette.visible = false
 		for child in ui_text_layer.get_children():
 			child.visible = false
 		update_currency()
 		visible = true
 	elif event.is_action_released("score_report"):
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
 		palette.visible = true
 		for child in ui_text_layer.get_children():
 			child.visible = true
