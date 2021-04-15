569c1b3cd06d3cb86d45487d6c98b39d991aad0b
Kalen Wallin <kwallin10@huskers.unl.edu>
2021-04-15 18:30:22 -0500

    All Info page variables are bound to TileMap script

--- Interface/InfoOverlayUIScript.gd
+++ Interface/InfoOverlayUIScript.gd
@@ -25,46 +25,91 @@ func _input(event):
 		get_node("TopRowLabels/House/HouseVPGain").text = str(buildings[11].vp)
 		get_node("TopRowLabels/House/HouseCostIncrement").text = "(+" + str(buildings[11].cost_increment) + ")"
 		get_node("TopRowLabels/House/HouseVPIncrement").text = "(+" + str(buildings[11].vp_increment) + ")"
+		_set_currency_interactions("Top", buildings, "House", 11, "Shop", 12)
+		_set_currency_interactions("Top", buildings, "House", 11, "Field", 15)
 		#update shop labels
 		get_node("TopRowLabels/Shop/ShopCost").text = str(buildings[12].cost)
 		get_node("TopRowLabels/Shop/ShopVPGain").text = str(buildings[12].vp)
 		get_node("TopRowLabels/Shop/ShopCostIncrement").text = "(+" + str(buildings[12].cost_increment) + ")"
 		get_node("TopRowLabels/Shop/ShopVPIncrement").text = "(+" + str(buildings[12].vp_increment) + ")"
+		_set_currency_interactions("Top", buildings, "Shop", 12, "House", 11)
+		_set_currency_interactions("Top", buildings, "Shop", 12, "Shop", 12)
+		_set_currency_interactions("Top", buildings, "Shop", 12, "BigHouse", 13)
+		_set_currency_interactions("Top", buildings, "Shop", 12, "Forge", 14)
+		_set_currency_interactions("Top",buildings, "Shop", 12, "Cathedral", 16)
 		#update big house labels
 		get_node("TopRowLabels/BigHouse/BigHouseCost").text = str(buildings[13].cost)
 		get_node("TopRowLabels/BigHouse/BigHouseVPGain").text = str(buildings[13].vp)
 		get_node("TopRowLabels/BigHouse/BigHouseCostIncrement").text = "(+" + str(buildings[13].cost_increment) + ")"
 		get_node("TopRowLabels/BigHouse/BigHouseVPIncrement").text = "(+" + str(buildings[13].vp_increment) + ")"
+		_set_currency_interactions("Top", buildings, "BigHouse", 13, "Shop", 12)
+		_set_vp_interactions("Top", buildings, "BigHouse", 13, "Cathedral", 16)
 		#update forge labels
 		get_node("TopRowLabels/Forge/ForgeCost").text = str(buildings[14].cost)
 		get_node("TopRowLabels/Forge/ForgeVPGain").text = str(buildings[14].vp)
 		get_node("TopRowLabels/Forge/ForgeCostIncrement").text = "(+" + str(buildings[14].cost_increment) + ")"
 		get_node("TopRowLabels/Forge/ForgeVPIncrement").text = "(+" + str(buildings[14].vp_increment) + ")"
+		_set_currency_interactions("Top", buildings, "Forge", 14, "Shop", 12)
+		_set_currency_interactions("Top", buildings, "Forge", 14, "Forge", 14)
+		_set_currency_interactions("Top", buildings, "Forge", 14, "Keep", 17)
+		_set_vp_interactions("Top", buildings, "Forge", 14, "Cathedral", 16)
 		#update field labels
 		get_node("BottomRowLabels/Field/FieldCost").text = str(buildings[15].cost)
 		get_node("BottomRowLabels/Field/FieldVPGain").text = str(buildings[15].vp)
 		get_node("BottomRowLabels/Field/FieldCostIncrement").text = "(+" + str(buildings[15].cost_increment) + ")"
 		get_node("BottomRowLabels/Field/FieldVPIncrement").text = "(+" + str(buildings[15].vp_increment) + ")"
+		_set_currency_interactions("Bottom", buildings, "Field", 15, "House", 11)
+		_set_currency_interactions("Bottom", buildings, "Field", 15, "Field", 15)
+		_set_vp_interactions("Bottom", buildings, "Field", 15, "Cathedral", 16)
 		#update cathedral labels
 		get_node("BottomRowLabels/Cathedral/CathedralCost").text = str(buildings[16].cost)
 		get_node("BottomRowLabels/Cathedral/CathedralVPGain").text = str(buildings[16].vp)
 		get_node("BottomRowLabels/Cathedral/CathedralCostIncrement").text = "(+" + str(buildings[16].cost_increment) + ")"
 		get_node("BottomRowLabels/Cathedral/CathedralVPIncrement").text = "(+" + str(buildings[16].vp_increment) + ")"
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "House", 11)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "Shop", 12)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "BigHouse", 13)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "Forge", 14)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "Field", 15)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "Cathedral", 16)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "Keep", 17)
+		_set_vp_interactions("Bottom", buildings, "Cathedral", 16, "Pyramid", 19)
 		#update keep labels
 		get_node("BottomRowLabels/Keep/KeepCost").text = str(buildings[17].cost)
 		get_node("BottomRowLabels/Keep/KeepVPGain").text = str(buildings[17].vp)
 		get_node("BottomRowLabels/Keep/KeepCostIncrement").text = "(+" + str(buildings[17].cost_increment) + ")"
 		get_node("BottomRowLabels/Keep/KeepVPIncrement").text = "(+" + str(buildings[17].vp_increment) + ")"
+		_set_currency_interactions("Bottom", buildings, "Keep", 17, "Forge", 14)
+		_set_vp_interactions("Bottom", buildings, "Keep", 17, "House", 11)
+		_set_vp_interactions("Bottom", buildings, "Keep", 17, "BigHouse", 13)
+		_set_vp_interactions("Bottom", buildings, "Keep", 17, "Field", 15)
+		_set_vp_interactions("Bottom", buildings, "Keep", 17, "Cathedral", 16)
+		_set_vp_interactions("Bottom", buildings, "Keep", 17, "Keep", 17)
+		_set_vp_interactions("Bottom", buildings, "Keep", 17, "Tower", 18)
 		#update tower labels
 		get_node("BottomRowLabels/Tower/TowerCost").text = str(buildings[18].cost)
 		get_node("BottomRowLabels/Tower/TowerVPGain").text = str(buildings[18].vp)
 		get_node("BottomRowLabels/Tower/TowerCostIncrement").text = "(+" + str(buildings[18].cost_increment) + ")"
 		get_node("BottomRowLabels/Tower/TowerVPIncrement").text = "(+" + str(buildings[18].vp_increment) + ")"
+		_set_currency_interactions("Bottom", buildings, "Tower", 18, "Forge", 14)
+		_set_vp_interactions("Bottom", buildings, "Tower", 18, "Field", 15)
+		_set_vp_interactions("Bottom", buildings, "Tower", 18, "Keep", 17)
+		_set_vp_interactions("Bottom", buildings, "Tower", 18, "Tower", 18)
+		_set_vp_interactions("Bottom", buildings, "Tower", 18, "Pyramid", 19)
 		#update pyramid labels
 		get_node("BottomRowLabels/Pyramid/PyramidCost").text = str(buildings[19].cost)
 		get_node("BottomRowLabels/Pyramid/PyramidVPGain").text = str(buildings[19].vp)
 		get_node("BottomRowLabels/Pyramid/PyramidCostIncrement").text = "(+" + str(buildings[19].cost_increment) + ")"
 		get_node("BottomRowLabels/Pyramid/PyramidVPIncrement").text = "(+" + str(buildings[19].vp_increment) + ")"
+		_set_currency_interactions("Bottom", buildings, "Pyramid", 18, "Forge", 14)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "House", 11)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "Shop", 12)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "BigHouse", 13)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "Field", 15)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "Cathedral", 16)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "Keep", 17)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "Tower", 18)
+		_set_vp_interactions("Bottom", buildings, "Pyramid", 19, "Pyramid", 19)
 		for child in ui_text_layer.get_children():
 			if child.name != "Timer":
 				child.visible = false
@@ -92,3 +137,19 @@ func _input(event):
 func _node_input_pause(node):
 	var nodeInput = node.is_processing_input()
 	node.set_process_input(!nodeInput)
+
+
+func _set_currency_interactions(row, buildings, buildingName, buildingNumber, interactionName, interactionNumber):
+	var currInt = buildings[buildingNumber].currency_interactions[interactionNumber]
+	if(currInt > 0):
+		get_node(row + "RowLabels/" + buildingName + "/" + interactionName + "CurrInt").text = '+' + str(currInt)
+	else:
+		get_node(row + "RowLabels/" + buildingName + "/" + interactionName + "CurrInt").text = str(currInt)
+
+
+func _set_vp_interactions(row, buildings, buildingName, buildingNumber, interactionName, interactionNumber):
+	var vpInt = buildings[buildingNumber].vp_interactions[interactionNumber]
+	if(vpInt > 0):
+		get_node(row + "RowLabels/" + buildingName + "/" + interactionName + "VPInt").text = '+' + str(vpInt)
+	else:
+		get_node(row + "RowLabels/" + buildingName + "/" + interactionName + "VPInt").text = str(vpInt)
