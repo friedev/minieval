ee82335706df1948da8dff5691be01efc12f5a4e
Kalen Wallin <kwallin10@huskers.unl.edu>
2021-04-18 20:13:06 -0500

    Able to change SFX Volume and In-game Music Volume

--- Interface/Options.gd
+++ Interface/Options.gd
@@ -23,7 +23,7 @@ func _on_ReturnToGame_pressed():
 
 
 func _on_CameraSpeed_value_changed(value):
-	var value_text = get_node("CameraSpeedValue")
+	var value_text = get_node("OptionsHeader/CameraSpeedValue")
 	Global.speed = value
 	value_text.text = str(value)
 
@@ -37,11 +37,30 @@ func _on_Options_ready():
 
 func _on_GameVolume_value_changed(value):
 	Global.music_volume = value
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), value)
 	var value_text = get_node("OptionsHeader/MusicValue")
 	value_text.text = str(value)
 	if value == 0:
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), -80)
+	elif value == 1:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), -40)
+	elif value == 2:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), -20)
+	elif value == 3:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), -10)
+	elif value == 4:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), -5)
+	elif value == 5:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), 0)
+	elif value == 6:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), 3)
+	elif value == 7:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), 6)
+	elif value == 8:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), 9)
+	elif value == 9:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), 12)
+	elif value == 10:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), 15)
 
 
 func _on_SFXVolume_value_changed(value):
@@ -51,6 +70,26 @@ func _on_SFXVolume_value_changed(value):
 	value_text.text = str(value)
 	if value == 0:
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -80)
+	elif value == 1:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -40)
+	elif value == 2:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -20)
+	elif value == 3:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -10)
+	elif value == 4:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), -5)
+	elif value == 5:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), 0)
+	elif value == 6:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), 3)
+	elif value == 7:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), 6)
+	elif value == 8:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), 9)
+	elif value == 9:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), 12)
+	elif value == 10:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), 15)
 
 
 func _on_Advanced_pressed():
