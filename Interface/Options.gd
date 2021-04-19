f2bc8ab9aa7845cc573b03bae5325b20d2f2ddf5
Aaron Friesen <maugrift@maugrift.com>
2021-04-19 03:01:13 -0500

    Very minor refactor of options menu code
    Also fit sliders into UI panel more nicely.

--- Interface/Options.gd
+++ Interface/Options.gd
@@ -8,6 +8,9 @@ onready var sfx_slider = $OptionsHeader/SFXVolume
 onready var game_slider = $OptionsHeader/GameVolume
 onready var options_header = $OptionsHeader
 
+onready var background_music = AudioServer.get_bus_index("Background music")
+onready var sound_effects = AudioServer.get_bus_index("Sound Effects")
+
 var volume_values = [-80, -40, -20, -10, -5, 0, 2, 4, 6, 8, 9]
 
 func _input(event):
@@ -31,8 +34,8 @@ func _on_Options_ready():
 
 func _on_GameVolume_value_changed(value):
 	Global.music_volume = value
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Background music"), volume_values[value])
+	AudioServer.set_bus_volume_db(background_music, volume_values[value])
 
 func _on_SFXVolume_value_changed(value):
 	Global.sfx_volume = value
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound Effects"), volume_values[value])
+	AudioServer.set_bus_volume_db(sound_effects, volume_values[value])
