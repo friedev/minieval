7716db499aae7f95f1cf288d79d8965894b6a6dd
Aaron Friesen <maugrift@maugrift.com>
2021-04-19 02:56:00 -0500

    Refactor timer, add support for hours

--- Timer.gd
+++ Timer.gd
@@ -1,17 +1,27 @@
 extends Node
 
 var timer
-var seconds: int = 0
-var minutes: int = 0
+var seconds_remaining: int = 0
+
+
+func update_label():
+	var hours = int(seconds_remaining / 3600)
+	var minutes = int(seconds_remaining / 60) % 60
+	var seconds = seconds_remaining % 60
+	if hours > 0:
+		$timeLeftLabel.set_text("%d:%02d:%02d" % [hours, minutes, seconds])
+	else:
+		$timeLeftLabel.set_text("%d:%02d" % [minutes, seconds])
+
 
 func _ready():
 	if Global.game_mode == 1: 
 		Global.timer_over = false
-		minutes = int(Global.game_time / 60)
-		seconds = Global.game_time % 60
-		$timeLeftLabel.set_text(str(minutes, ":", str(seconds).pad_zeros(2)))
+		seconds_remaining = Global.game_time
+		update_label()
+		
 		timer = Timer.new()
-		timer.connect("timeout",self,"_on_timer_timeout") 
+		timer.connect("timeout", self, "_on_timer_timeout") 
 		timer.set_wait_time(1)
 		timer.set_one_shot(false)
 		add_child(timer) 
@@ -19,13 +29,9 @@ func _ready():
 
 
 func _on_timer_timeout():
-	if seconds == 0 and minutes == 0:
+	seconds_remaining -= 1
+	update_label()
+	
+	if seconds_remaining == 0:
 		Global.timer_over = true
-	if seconds == 0:
-		seconds = 60
-		minutes -= 1
-	seconds -= 1
-	if minutes < 0:
-		minutes = 0
-		seconds = 0
-	$timeLeftLabel.set_text(str(minutes, ":", str(seconds).pad_zeros(2)))
+		timer.queue_free()
