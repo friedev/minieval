2da467cee006412f8fd852eb9fa8dfd1e7408258
Aaron Friesen <aaron@frie.dev>
2022-10-10 20:58:55 -0500

    Use rich text to color interactions on tooltip

--- scripts/Palette.gd
+++ scripts/Palette.gd
@@ -2,6 +2,13 @@ extends TileMap
 
 signal palette_selection(id)
 
+const GP_STR := "GP"
+const GP_COLOR := Color(1.0, 0.75, 0.0)
+const VP_STR := "VP"
+const VP_COLOR := Color(0.0, 0.75, 1.0)
+const POSITIVE_COLOR := Color(0.0, 1.0, 0.0)
+const NEGATIVE_COLOR := Color(1.0, 0.0, 0.0)
+
 onready var tooltip := get_node("../Tooltip")
 onready var name_label := tooltip.get_node("VBoxContainer/NameLabel")
 onready var gp_label := tooltip.get_node("VBoxContainer/GPContainer/GPLabel")
@@ -14,37 +21,90 @@ onready var building_ids: Array = tile_map.BUILDINGS.keys()
 onready var tooltip_position: Vector2 = tooltip.rect_position
 var last_tooltip_id := -1
 
-
 func _ready():
 	building_ids.remove(tile_map.EMPTY)
 	building_ids.sort()
 
 
-func get_interaction_str(value: int, resource: String) -> String:
-	var value_str: String = ("+%d" if value > 0 else "%d") % value
-	return "%s %s" % [value_str, resource]
+# Returns plaintext string
+func push_interaction_value(value: int, type: String, type_color: Color) -> String:
+	var length := 0
+	var string: String
+	if value > 0:
+		interactions_label.push_color(POSITIVE_COLOR)
+		string = "+%d" % value
+	else:
+		interactions_label.push_color(NEGATIVE_COLOR)
+		string = "%d" % value
+	interactions_label.add_text(string)
+	interactions_label.pop()
+
+	interactions_label.add_text(" ")
+	string += " "
+
+	interactions_label.push_color(type_color)
+	interactions_label.add_text(type)
+	string += type
+	interactions_label.pop()
+
+	return string
 
 
-func get_interactions_str(id: int, gp: int, vp: int) -> String:
-	var building = buildings[id]
-	var interaction_strings := []
+# Returns plaintext string
+func push_interaction(building_str: String, gp: int, vp: int) -> String:
+	var string: String = "%s: " % building_str
+	interactions_label.add_text(string)
+
 	if gp != 0:
-		interaction_strings.append(get_interaction_str(gp, "GP"))
+		string += push_interaction_value(gp, GP_STR, GP_COLOR)
+
 	if vp != 0:
-		interaction_strings.append(get_interaction_str(vp, "VP"))
-	var interactions_string := ", ".join(interaction_strings)
-	return "%s: %s\n" % [building.name, interactions_string]
+		if gp != 0:
+			interactions_label.add_text(", ")
+			string += ", "
+
+		string += push_interaction_value(vp, VP_STR, VP_COLOR)
+
+	return string
 
 
-func get_interactions_text(building) -> String:
-	var interactions_text := ""
+# Returns array of plaintext strings
+func push_all_interactions(id: int) -> Array:
+	var building = buildings[id]
+	if id == tile_map.PYRAMID:
+		# Hack to avoid printing a line for every pyramid interaction
+		return [
+			push_interaction(
+				"ANY",
+				building.gp_interactions[tile_map.HOUSE],
+				building.vp_interactions[tile_map.HOUSE]
+			)
+		]
+
+	var first := true
+	var lines := []
 	for other_id in building_ids:
 		var gp: int = building.gp_interactions.get(other_id, 0)
 		var vp: int = building.vp_interactions.get(other_id, 0)
 		if gp == 0 and vp == 0:
 			continue
-		interactions_text += get_interactions_str(other_id, gp, vp)
-	return interactions_text.rstrip("\n")
+
+		if first:
+			first = false
+		else:
+			interactions_label.newline()
+
+		lines.append(push_interaction(buildings[other_id].name, gp, vp))
+	return lines
+
+
+func max_line_width(lines: Array) -> float:
+	var max_width := 0.0
+	for line in lines:
+		var width: float = interactions_label.get_font("normal_font").get_string_size(line).x
+		if width > max_width:
+			max_width = width
+	return max_width
 
 
 func update_tooltip(id: int) -> void:
@@ -52,17 +112,17 @@ func update_tooltip(id: int) -> void:
 	name_label.text = building.name
 	gp_label.text = str(-building.gp)
 	vp_label.text = str(building.vp)
-	var interactions_text: String
-	if id == tile_map.PYRAMID:
-		# Hack to avoid printing a line for every pyramid interaction
-		interactions_text = "ANY: %s, %s" % [
-			get_interaction_str(building.gp_interactions[tile_map.HOUSE], "GP"),
-			get_interaction_str(building.vp_interactions[tile_map.HOUSE], "VP"),
-		]
+	interactions_label.clear()
+	var lines := push_all_interactions(id)
+	if len(lines) > 0:
+		interactions_label.show()
+		# Dynamically resize interactions_label
+		# RichTextLabel does not have fit_content_width or get_content_width()
+		# Instead, determine the length of the string (in plain text) in the chosen font
+		interactions_label.rect_min_size.x = max_line_width(lines)
+		print(interactions_label.rect_min_size.x)
 	else:
-		interactions_text = get_interactions_text(building)
-	interactions_label.text = interactions_text
-	interactions_label.visible = len(interactions_text) > 0
+		interactions_label.hide()
 
 
 func _input(event: InputEvent):
@@ -88,7 +148,7 @@ func _input(event: InputEvent):
 
 	# Hack to map nice road icon (21) to actual road ID (2)
 	if id == 21:
-		id = 2
+		id = tile_map.ROAD
 
 	if hover:
 		tooltip.visible = true
