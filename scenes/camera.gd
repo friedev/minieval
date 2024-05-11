3511a7f3edf073a9d770d42b063c774fd2e4310d
Aaron Friesen <aaron@frie.dev>
2024-05-11 14:36:16 -0500

    Remove camera offset to simplify camera zooming

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -1,7 +1,6 @@
 extends Camera2D
 
 @onready var default_zoom := self.zoom
-@onready var default_offset := self.offset
 
 
 func _process(delta: float) -> void:
@@ -17,21 +16,13 @@ func _process(delta: float) -> void:
 
 func reset_zoom() -> void:
 	self.zoom = self.default_zoom
-	self.position += self.offset - self.default_offset
-	self.offset = self.default_offset
 
 
 func zoom_by(zoomfactor: float) -> void:
 	var new_zoom_level := (self.zoom * zoomfactor).x
 #	if new_zoom_level > 1 or new_zoom_level < 0.0625:
 #		return
-
 	self.zoom *= zoomfactor
-	# Compensate for the upcoming offset change by adjusting the position
-	# This way, the camera zooms out from the center rather than the top left
-	self.position += offset - offset * zoomfactor
-	# Update offset so that the camera's origin remains at the top left
-	self.offset *= zoomfactor
 
 
 func _input(event: InputEvent) -> void:
