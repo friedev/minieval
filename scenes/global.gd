3726635283db731108bcd98e4d9355f68b7c6973
Aaron Friesen <aaron@frie.dev>
2024-05-12 16:12:13 -0500

    Replace Global with self in global.gd

--- scenes/global.gd
+++ scenes/global.gd
@@ -15,34 +15,34 @@ const SOUND_VOLUME := 1.0
 
 var endless := false:
 	set(value):
-		if value != Global.endless:
+		if value != self.endless:
 			endless = value
-			Global.endless_changed.emit(Global.endless)
+			self.endless_changed.emit(self.endless)
 
-var num_turns := Global.NUM_TURNS
-var game_size := Global.GAME_SIZE
-var initial_gp := Global.INITIAL_GP
+var num_turns := self.NUM_TURNS
+var game_size := self.GAME_SIZE
+var initial_gp := self.INITIAL_GP
 var tutorial_seen := false
 
 var is_menu_open := false:
 	set(value):
-		if value != Global.is_menu_open:
+		if value != self.is_menu_open:
 			is_menu_open = value
-			self.get_tree().paused = Global.is_menu_open
-			Global.is_menu_open_changed.emit(Global.is_menu_open)
+			self.get_tree().paused = self.is_menu_open
+			self.is_menu_open_changed.emit(self.is_menu_open)
 
 # Options
-var speed := Global.SPEED
-var music_volume := Global.MUSIC_VOLUME
-var sound_volume := Global.SOUND_VOLUME
+var speed := self.SPEED
+var music_volume := self.MUSIC_VOLUME
+var sound_volume := self.SOUND_VOLUME
 
 
 func reset_game_parameters() -> void:
-	Global.num_turns = Global.NUM_TURNS
-	Global.game_size = Global.GAME_SIZE
+	self.num_turns = self.NUM_TURNS
+	self.game_size = self.GAME_SIZE
 
 
 func change_scene_to_file(path: String) -> void:
-	Global.is_menu_open = false
+	self.is_menu_open = false
 	self.get_tree().paused = false
 	self.get_tree().change_scene_to_file(path)
